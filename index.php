501907cd85082a305be372313d780b1491819a60
philemmons <phil_emmons@sbcglobal.net>
2025-04-21 12:08:14 -0700

    content;layout;base structure;g-analytics;cookies

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();  //start or resume an existing session
+include_once 'header-top.inc';
+echo "<title>Website Design | OnDemand IT</title>";
+include_once 'header-bottom.inc';
+?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Website Design | OnDemand IT</title>
+<body id="toTop">
+    <a href="#main-content" class="btn btn-primary btn-skip" aria-label="Go To Main Content">Go To Main Content</a>
 
-    <meta name="description" content="Experienced in web development and maintenance, creating WCAG 2.1-compliant, usable, secure, user-focused websites.">
-    <meta name="author" content="SitePoint">
-
-    <meta property="og:title" content="Website Design | OnDemand IT">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://on-demand.philemmons.dev/">
-    <meta property="og:description" content="OnDemand IT Services delivers expert tech support, SEO-optimized websites, and powerful online surveys—helping your business run smarter, faster.">
-    <meta property="og:image" content="/images/one-zero.jpg">
-
-    <link rel="icon" href="/images/icons/favicon.ico">
-    <link rel="apple-touch-icon" href="/images/icons/apple-touch-icon.png">
-
-    <link rel="stylesheet" href="css/styles.css">
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-</head>
-
-<body>
     <h1>Hello, world!</h1>
-
-
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 </body>
+
+
+
 
 </html>
