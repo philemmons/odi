e2c093390d0c0704032d06038c9eb24c43e12550
philemmons <phil_emmons@sbcglobal.net>
2025-04-21 18:35:32 -0700

    content; layout; css; img

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include_once 'header-bottom.inc';
 
   <!-- Hero Section -->
   <header id="main-content">
-    <div class="py-7 px-5 text-center bg-img-hero">
+    <div class="py-6 px-5 text-center bg-img-hero">
       <div class="container-fluid py-5">
         <h1 class="py-1 text-white">Website Design | OnDemand IT</h1>
         <div class="h-bar"></div>
@@ -19,6 +19,39 @@ include_once 'header-bottom.inc';
       </div>
     </div>
   </header>
+
+
+  <nav class="navbar navbar-expand-lg fixed-top" aria-label="main menu">
+    <div class="container-xxl">
+      <a class="navbar-brand" href="/">
+        <p class="odit-logo-title"><img src="/images/icons/favicon-32x32.png" alt="Flip-flop circuit token" class="pe-2">OnDemand IT</p>
+
+      </a>
+      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#topNavbar" aria-controls="topNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="pe-2">MENU</span>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="topNavbar">
+        <ul class="navbar-nav ms-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active px-2" href="index.php" aria-current="page">Home<span class="visually-hidden">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-2" href="our_story.php">Our Story</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-2" href="purchase.php">Purchase</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-2" href="newsletter-april-2024.php">Newsletter</a>
+          </li>
+          <li class="nav-item" style="border-right: none;">
+            <a class="nav-link px-2" href="contact.php">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
 
 
